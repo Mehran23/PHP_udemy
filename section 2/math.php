813a592ebb6850fdf38f3391ce2905b8cd1f64e5
Mehran23 <amster2almere@outlook.com>
2021-06-09 10:16:11 +0200

    section 2
    g

---
+++ section 2/math.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    
+    echo 56 + 20;
+    echo "<br>";
+    echo 56 - 45;
+    echo "<br>";
+    echo 8 * 8;
+    echo "<br>";
+    echo 90 / 10;
+    echo "<br>";
+    echo "<br>";
+
+
+
+    echo 60 + 50 * 8 / 80;
+    echo "<br>";
+    echo (5 + 6) * 10;
+    echo "<br>";
+
+
+    $number1= 20;
+    $number2= 40;
+
+    echo $number1 + $number2;
+
+
+?>
+</body>
+</html>
